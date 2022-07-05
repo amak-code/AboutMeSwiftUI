9ec199bc0661a75ea2e8e97e364dae2ad3cff3de
Aleksandra Makhrova <amakhro@gmail.com>
2022-07-05 11:11:40 -0700

    Initial Commit

---
+++ SwiftUiDemo/SwiftUiDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiDemoApp.swift
+//  SwiftUiDemo
+//
+//  Created by antikiller on 05.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
